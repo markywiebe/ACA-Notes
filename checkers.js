59dd9d4bb31b60749b44d9edaa9c8fd497b0b270
Marcus Wiebe <mgwiebe@comcast.net>
2015-10-08 19:34:50 -0500

    initial checker commit

---
+++ checkers.js
@@ -0,0 +1,33 @@
+function Checker(color, position) {
+	this.color = color;
+	this.position = position;
+}
+
+function Board() {
+	this.grid = 
+	[ 
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	[],[],[],[],[],[],[],[],[]
+	 ]
+
+	this.checkers = [ ]
+	this.createCheckers = function() {
+		var whitePositions = [[0,1], [0,3], [0,5], [0,7], [1,0]... ]
+		var redPositions = [[5,0], [5,2], [5,4], [5,6], [5,8]...]
+		for (var i = 0; i < 12; i++) {
+			this.checkers.push(new Checker('white', whitePositions[i]))
+			this.checkers.push(new Checker('red', redPositions[i]))
+		}
+	}
+	this.placeCheckers = function() {
+		var checker1 = this.checkers[0]
+		var checkerPosition = checker1.position
+		this.grid[0][1] = checker1
+	}
+}
